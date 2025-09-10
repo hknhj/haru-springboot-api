9ad43113e04affac09380d6c082997f4ccfec8fe
이석주 <nick981213@naver.com>
2025-09-10 14:56:25 +0900

    refactor/#59: getMeetingProceeding 테스트 코드 작성

--- src/test/java/com/haru/api/meeting/application/service/MeetingQueryUseCaseImplTest.java
+++ src/test/java/com/haru/api/meeting/application/service/MeetingQueryUseCaseImplTest.java
@@ -6,7 +6,7 @@ import com.haru.api.meeting.domain.Meeting;
 import com.haru.api.meeting.presentation.dto.MeetingResponseDTO;
 import com.haru.api.user.domain.User;
 import com.haru.api.workspace.domain.Workspace;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,13 +15,11 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MeetingQueryUseCaseImplTest {
@@ -32,58 +30,73 @@ class MeetingQueryUseCaseImplTest {
     @Mock
     private MeetingPort meetingPort;
 
+    private User user1;
+    private User user2;
+    private Workspace workspace;
+    private Meeting meeting1;
+    private Meeting meeting2;
+    private List<Meeting> meetings;
+
+    @BeforeEach
+    void setUp() {
+        user1 = User.builder()
+                .id(1L)
+                .name("testUser")
+                .build();
+
+        user2 = User.builder()
+                .id(5L)
+                .name("testCreator")
+                .build();
+
+        workspace = Workspace.builder()
+                .id(100L)
+                .title("testWorkspace")
+                .build();
+
+        meeting1 = Meeting.builder()
+                .id(1L)
+                .title("회의1")
+                .workspace(workspace)
+                .creator(user1)
+                .build();
+        meeting2 = Meeting.builder()
+                .id(2L)
+                .title("회의2")
+                .workspace(workspace)
+                .creator(user2)
+                .build();
+        meetings = List.of(meeting1, meeting2);
+    }
+
     @Test
     @DisplayName("회의 목록 조회 성공")
     void getMeetings_Success() {
 
         // given
-
-        // 테스트용 데이터
-        User user = User.builder()
-                .id(1L)
-                .name("testUser")
-                .build();
-
-        Workspace workspace = Workspace.builder()
-                .id(100L)
-                .title("testWorkspace")
-                .build();
-
-        Meeting meeting1 = Meeting.builder()
-                .id(1L)
-                .title("회의1")
-                .workspace(workspace)
-                .build();
-        Meeting meeting2 = Meeting.builder()
-                .id(2L)
-                .title("회의2")
-                .workspace(workspace)
-                .build();
-        List<Meeting> meetings = List.of(meeting1, meeting2);
-
         // 반환될 DTO 객체
         MeetingResponseDTO.getMeetingResponse response1 = MeetingResponseDTO.getMeetingResponse.builder()
                 .meetingId(1L)
-                .title("회의1")
+                .title(meeting1.getTitle())
                 .isCreator(true)
                 .build();
         MeetingResponseDTO.getMeetingResponse response2 = MeetingResponseDTO.getMeetingResponse.builder()
                 .meetingId(2L)
-                .title("회의2")
+                .title(meeting2.getTitle())
                 .isCreator(false)
                 .build();
         List<MeetingResponseDTO.getMeetingResponse> expectedResponses = List.of(response1, response2);
 
         // mock 객체 동작 정의
-        when(meetingPort.findAllByWorkspaceIdOrderByUpdatedAtDesc(workspace.getId())).thenReturn(meetings);
+        given(meetingPort.findAllByWorkspaceIdOrderByUpdatedAtDesc(workspace.getId())).willReturn(meetings);
 
         // MeetingConverter의 정적(static) 메서드 모킹
         try (MockedStatic<MeetingConverter> mockedConverter = mockStatic(MeetingConverter.class)) {
-            mockedConverter.when(() -> MeetingConverter.toGetMeetingResponse(meeting1, user.getId())).thenReturn(response1);
-            mockedConverter.when(() -> MeetingConverter.toGetMeetingResponse(meeting2, user.getId())).thenReturn(response2);
+            mockedConverter.when(() -> MeetingConverter.toGetMeetingResponse(meeting1, user1.getId())).thenReturn(response1);
+            mockedConverter.when(() -> MeetingConverter.toGetMeetingResponse(meeting2, user1.getId())).thenReturn(response2);
 
             // when
-            List<MeetingResponseDTO.getMeetingResponse> result = meetingQueryUseCase.getMeetings(user, workspace);
+            List<MeetingResponseDTO.getMeetingResponse> result = meetingQueryUseCase.getMeetings(user1, workspace);
 
             // then
             assertThat(result).isNotNull();
@@ -92,8 +105,8 @@ class MeetingQueryUseCaseImplTest {
 
             // Mock 객체가 예상대로 호출되었는지 검증
             verify(meetingPort, times(1)).findAllByWorkspaceIdOrderByUpdatedAtDesc(workspace.getId());
-            mockedConverter.verify(() -> MeetingConverter.toGetMeetingResponse(meeting1, user.getId()), times(1));
-            mockedConverter.verify(() -> MeetingConverter.toGetMeetingResponse(meeting2, user.getId()), times(1));
+            mockedConverter.verify(() -> MeetingConverter.toGetMeetingResponse(meeting1, user1.getId()), times(1));
+            mockedConverter.verify(() -> MeetingConverter.toGetMeetingResponse(meeting2, user1.getId()), times(1));
         }
     }
 
@@ -102,25 +115,41 @@ class MeetingQueryUseCaseImplTest {
     void getMeetings_WhenNoMeetings_ReturnsEmptyList() {
 
         // given
-        User user = User.builder()
-                .id(1L)
-                .name("testUser")
-                .build();
-
-        Workspace workspace = Workspace.builder()
-                .id(100L)
-                .title("testWorkspace")
-                .build();
-
-        when(meetingPort.findAllByWorkspaceIdOrderByUpdatedAtDesc(workspace.getId())).thenReturn(Collections.emptyList());
+        given(meetingPort.findAllByWorkspaceIdOrderByUpdatedAtDesc(workspace.getId())).willReturn(Collections.emptyList());
 
         // when
-        List<MeetingResponseDTO.getMeetingResponse> result = meetingQueryUseCase.getMeetings(user, workspace);
+        List<MeetingResponseDTO.getMeetingResponse> result = meetingQueryUseCase.getMeetings(user1, workspace);
 
         // then
         assertThat(result).isNotNull();
         assertThat(result).isEmpty();
 
         verify(meetingPort, times(1)).findAllByWorkspaceIdOrderByUpdatedAtDesc(workspace.getId());
+    }
+
+    @Test
+    @DisplayName("회의록 상세 조회 성공")
+    void getMeetingProceeding_Success() {
+
+        // given
+        MeetingResponseDTO.getMeetingProceeding expectedResponse = MeetingResponseDTO.getMeetingProceeding.builder()
+                .userId(user1.getId())
+                .workspaceId(workspace.getId())
+                .build();
+
+        try (MockedStatic<MeetingConverter> mockedConverter = mockStatic(MeetingConverter.class)) {
+            mockedConverter.when(() -> MeetingConverter.toGetMeetingProceedingResponse(user1, meeting1))
+                    .thenReturn(expectedResponse);
+
+            // when
+            MeetingResponseDTO.getMeetingProceeding result = meetingQueryUseCase.getMeetingProceeding(user1, meeting1);
+
+            // then
+            assertThat(result).isNotNull();
+            assertThat(result).isEqualTo(expectedResponse); // 반환된 결과가 기대한 DTO와 일치하는지 확인
+
+            // Mock 객체들이 예상대로 호출되었는지 검증
+            mockedConverter.verify(() -> MeetingConverter.toGetMeetingProceedingResponse(user1, meeting1), times(1));
+        }
     }
 }
